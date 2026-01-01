99070ab647050bce38909a6acd98888c77ab01dd
Krzysztof Staroń <kisiel3141@gmail.com>
2026-01-01 15:59:46 +0100

    update root lockfile for turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["d3", "pdfjs-dist"],
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
