312824df41badac2106f1ce647df6e8d01d2e7b8
krzysztofstaron <kisiel3141@gmail.com>
2026-01-08 20:59:52 +0100

    analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import "./globals.css";
 import { ReduxProvider } from "./store/ReduxProvider";
 import { GlobalErrorHandler } from "./components/GlobalErrorHandler";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${universalSans.variable} antialiased`}
       >
+        <Analytics />
         <GlobalErrorHandler>
           <ReduxProvider>{children}</ReduxProvider>
         </GlobalErrorHandler>
